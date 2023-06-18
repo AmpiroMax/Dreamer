5dd7314ba804e469c27124b7e1d33912957b583b
AmpiroMax <patrat2015@yandex.ru>
2023-06-18 12:28:32 +0300

    L9 | keybinds were added

--- Entity.cpp
+++ Entity.cpp
@@ -13,20 +13,7 @@ void Entity::move(const float& dt, const float dx, const float dy)
     this->shape.move(dx * this->movementSpeed * dt, dy * this->movementSpeed * dt);
 }
 
-void Entity::update(const float& dt)
-{
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-        this->move(dt, -1.f, 0.f);
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-        this->move(dt, 1.f, 0.f);
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-        this->move(dt, 0.f, -1.f);
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-        this->move(dt, 0.f, 1.f);
-}
+void Entity::update(const float& dt) {}
 
 void Entity::render(sf::RenderTarget* target)
 {
--- Game.cpp
+++ Game.cpp
@@ -30,9 +30,20 @@ void Game::initWindow()
     this->window->setVerticalSyncEnabled(vertical_sync_enabled);
 }
 
+void Game::initKeys()
+{
+    this->supportedKeys.emplace("Escape", sf::Keyboard::Key::Escape);
+    this->supportedKeys.emplace("A", sf::Keyboard::Key::A);
+    this->supportedKeys.emplace("D", sf::Keyboard::Key::D);
+    this->supportedKeys.emplace("W", sf::Keyboard::Key::W);
+    this->supportedKeys.emplace("S", sf::Keyboard::Key::S);
+
+    std::cout << this->supportedKeys.at("A") << "\n";
+}
+
 void Game::initStates()
 {
-    this->states.push(new GameState(this->window));
+    this->states.push(new GameState(this->window, &this->supportedKeys));
 }
 
 // Constructors/Destructors
@@ -40,6 +51,7 @@ void Game::initStates()
 Game::Game()
 {
     this->initWindow();
+    this->initKeys();
     this->initStates();
 }
 
